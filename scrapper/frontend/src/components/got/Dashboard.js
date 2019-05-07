dcf7a10ad47773d1254de01f149fdfe4b4f7d5fb
Pedro Costa <pedrobc89@gmail.com>
2019-05-06 21:05:49 -0300

    Table show of the scrapped items

--- scrapper/frontend/src/components/got/Dashboard.js
+++ scrapper/frontend/src/components/got/Dashboard.js
@@ -1,12 +1,12 @@
-import React, {Fragment} from 'react';
-import Form from './Form';
-import Got from './Got';
+import React, { Fragment } from "react";
+import Form from "./Form";
+import Got from "./Got";
 
 export default function Dashboard() {
   return (
     <Fragment>
-        <Form />
-        <Got />
+      {/* <Form /> */}
+      <Got />
     </Fragment>
-  )
+  );
 }
--- scrapper/frontend/src/components/got/Got.js
+++ scrapper/frontend/src/components/got/Got.js
@@ -21,27 +21,18 @@ export class Got extends Component {
         <table className="table table-stripped">
           <thead>
             <tr>
-              <th>ID</th>
               <th>Name</th>
-              <th>Email</th>
-              <th>Message</th>
-              <th />
+              <th>Price</th>
+              <th>Img</th>
             </tr>
           </thead>
           <tbody>
             {this.props.got.map(got => (
               <tr key={got.id}>
-                <td>{got.id}</td>
                 <td>{got.name}</td>
-                <td>{got.email}</td>
-                <td>{got.message}</td>
+                <td>{got.price}</td>
                 <td>
-                  <button
-                    onClick={this.props.deleteGot.bind(this, got.id)}
-                    className="btn btn-danger btn-sm"
-                  >
-                    Delete
-                  </button>
+                  <img src={got.img} height="300" />
                 </td>
               </tr>
             ))}
